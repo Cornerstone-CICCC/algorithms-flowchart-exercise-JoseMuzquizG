6464a87842376983e0fe4c14f2bba24f849ea75a
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-09-17 11:51:28 -0700

    Added js code for exercise 3

---
+++ js/exercise3.js
@@ -0,0 +1,20 @@
+let a = 12
+let b = 35
+let c = 13
+
+if (a > b) {
+    if (a > c) {
+        console.log(`"a" (${a}) is the largest number`)
+    }
+    else {
+        console.log(`"c" (${c}) is the largest number`)
+    }
+}
+else {
+    if (b > c) {
+        console.log(`"b" (${b}) is the largest number`)
+    }
+    else {
+        console.log(`"c" (${c}) is the largest number`)
+    }
+}
